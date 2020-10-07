dbd908bf39e15d36270d8c8f2810ba5e25d56a4b
Gerch17 <tambovtsevy228@mail.ru>
2020-10-07 17:27:02 +0300

    Fixes

---
+++ src/ru/gerch/ex8/twentyShapes/Painter.java
@@ -0,0 +1,58 @@
+package ru.gerch.ex8.twentyShapes;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.Random;
+
+public class Painter extends JFrame {
+
+    private static final Random random = new Random();
+    public static final int width = 800;
+    public static final int height = 600;
+    public static int x = 10;
+    public static int y = 20;
+
+
+    public static Color randomColor(){
+        int r = random.nextInt(256);
+        int g = random.nextInt(256);
+        int b = random.nextInt(256);
+        return new Color(r, g, b);
+    }
+    public static Shape randomShape(){
+        Color color = randomColor();
+        int nx = x;
+        int ny = y;
+        if (x + 100 <= width - 100){
+            x += 75;
+        } else {
+            x = 10;
+            y += 100;
+        }
+        int w = random.nextInt(50) + 50;
+        int h = random.nextInt(50) + 25;
+        switch (random.nextInt(3)){
+            case 0:
+                return new Circle(nx, ny, color, w / 2);
+            case 1:
+                return new Rectangle(nx, ny, color, w, h);
+            case 2:
+                return  new Square(nx, ny, color, w);
+        }
+        return new Circle(10, 10, color, w * 2);
+    }
+    public Painter(){
+        super("Task 1");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setDefaultLookAndFeelDecorated(true);
+        JLayeredPane pane = getLayeredPane();
+        for (int i = 0; i < 20; i++){
+            Shape shape = randomShape();
+            shape.setBounds(0, 0, width, height);
+            pane.add(shape);
+        }
+        setSize(width, height);
+        setVisible(true);
+    }
+
+}
--- src/ru/gerch/ex8/twentyShapes/Test.java
+++ src/ru/gerch/ex8/twentyShapes/Test.java
@@ -5,58 +5,7 @@ import java.awt.*;
 import java.util.Random;
 
 class Main extends JFrame {
-
-    private static final Random random = new Random();
-    public static final int width = 800;
-    public static final int height = 600;
-    public static int x = 10;
-    public static int y = 20;
-
-
-    public static Color randomColor(){
-        int r = random.nextInt(256);
-        int g = random.nextInt(256);
-        int b = random.nextInt(256);
-        return new Color(r, g, b);
-    }
-    public static Shape randomShape(){
-        Color color = randomColor();
-        int nx = x;
-        int ny = y;
-        if (x + 100 <= width - 100){
-            x += 75;
-        } else {
-            x = 10;
-            y += 100;
-        }
-        int w = random.nextInt(50) + 50;
-        int h = random.nextInt(50) + 25;
-        switch (random.nextInt(3)){
-            case 0:
-                return new Circle(nx, ny, color, w / 2);
-            case 1:
-                return new Rectangle(nx, ny, color, w, h);
-            case 2:
-                return  new Square(nx, ny, color, w);
-        }
-        return new Circle(10, 10, color, w * 2);
-    }
-    public Main(){
-        super("Task 1");
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setDefaultLookAndFeelDecorated(true);
-        JLayeredPane pane = getLayeredPane();
-        for (int i = 0; i < 20; i++){
-            Shape shape = randomShape();
-            shape.setBounds(0, 0, width, height);
-            pane.add(shape);
-        }
-        setSize(width, height);
-        setVisible(true);
-
-    }
-
     public static void main(String[] args) {
-        new Main();
+        Painter painter = new Painter();
     }
 }
